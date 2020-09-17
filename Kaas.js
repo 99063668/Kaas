c7a1320f1933b7a872bb2d75e112dc6ab4317182
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-09-17 11:46:57 +0200

    Add files via upload

--- Kaas.js
+++ Kaas.js
@@ -1,15 +1,53 @@
-//Vraag 1:
-document.write('<br>Is de kaas geel? : ' + prompt('Is de kaas geel?'));
-var antwoord = ja;
-var vraag;
+var antwoord;
 
-if(antwoord == ja){
+antwoord = prompt("is de kaas geel?");
 
-    vraag = true;
-    document.write('<br>Zitten er gaten in? : ' + prompt('Zitten er gaten in?'));
+if (antwoord == "ja"){
+    document.write("De kleur is geel.<br>");
+
+    antwoord = prompt("Heeft de gaten?");
+    if (antwoord == "ja"){
+        document.write("De kaas heeft gaten.<br>");
+    }else{
+        document.write("De kaas heeft geen gaten.<br>");
+    }
+                antwoord = prompt("Is de kaas belachelijk duur");
+        if (antwoord == "ja"){
+             document.write("Emmenthaler.<br>");
+       }else{
+            document.write("Leerdammer.<br>");
+       }
+   
+                    antwoord = prompt("Is de kaas hard als steen");
+             if (antwoord == "ja"){
+                     document.write("Pannigiano Reggiano.<br>");
+            }else{
+                document.write("Goudse kaas.<br>");
+   }
+             antwoord = prompt("Is de kaas belachelijk duur");
+         if (antwoord == "ja"){
+              document.write("Emmenthaler.<br>");
+        }else{
+             document.write("Leerdammer.<br>");
+        }
 
 }else{
-
-    vraag = false;
-    document.write('<br>Heeft de kaas blauwe schimmels? : ' + prompt('Heeft de kaas blauwe schimmels?'));
+    document.write("De kleur niet is geel.<br>");
 }
+    antwoord = prompt("Heeft de kaas blauwe schimmels?");
+    if (antwoord == "ja"){
+        document.write("De kaas heeft blauwe schimmels.<br>");
+    }else{
+        document.write("De kaas heeft geen blauwe schimmels.<br>");
+
+    }       antwoord = prompt("Heeft de kaas een korst?");
+        if (antwoord == "ja"){
+         document.write("Camenbert.<br>");
+     }else{
+         document.write("Mozzarella.<br>");
+     }           antwoord = prompt("Heeft de kaas een korst?");
+     if (antwoord == "ja"){
+      document.write("Blue de Rochbaron.<br>");
+  }else{
+      document.write("Founne d' Ambert.<br>");
+  }
